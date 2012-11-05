ec4ae3046f4fc0861d819c219a00351af4c776f1
Paul Schuegraf <paul@verticallabs.ca>
2012-11-05 10:58:51 -0800

    removed railtie - should do this manually

--- lib/delayed_mail/railtie.rb
+++
@@ -1,9 +0,0 @@
-require 'rails'
-require 'delayed/mail_delivery'
-
-class DelayedMailRailTie < Rails::Railtie
-  config.before_configuration do
-    Delayed::MailDelivery.setup(:smtp)
-    config.action_mailer.delivery_method = :delayed
-  end
-end
